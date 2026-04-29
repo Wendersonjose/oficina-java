1dee91c7d5e45e1cef5fca26a7f248845b18970e
Wenderson José <wenderson.teles@icloud.com>
2026-04-29 18:04:58 -0300

    feat: implementa classe Servico com descricao e valor

--- src/br/com/wenderson/oficina/modelos/Servico.java
+++ src/br/com/wenderson/oficina/modelos/Servico.java
@@ -1,4 +1,31 @@
 package br.com.wenderson.oficina.modelos;
 
 public class Servico {
+    private String descricao;
+    private double valor;
+
+    public Servico(String descricao, double valor) {
+        if (descricao == null || descricao.isBlank()) {
+            throw new IllegalArgumentException("Descrição inválida");
+        }
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor deve ser maior que zero");
+        }
+
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return descricao + " - R$ " + String.format("%.2f", valor);
+    }
 }
